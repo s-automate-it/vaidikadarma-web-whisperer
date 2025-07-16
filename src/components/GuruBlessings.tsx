4c47339a5a0475ebec2d28c1f353f6de5563eccd
Automateit2024 <s.automateit@gmail.com>
2025-07-16 15:59:31 +0530

    Update GuruBlessings.tsx

--- src/components/GuruBlessings.tsx
+++ src/components/GuruBlessings.tsx
@@ -20,9 +20,11 @@ export const GuruBlessings = () => {
                 alt="Venkata Shivanaga Kumar" 
                 className="w-full h-full object-cover"
               />
-              <h3 className="text-2xl md:text-3xl font-bold text-white mb-2">
+              <div>
+              <h2 className="text-2xl md:text-3xl font-bold text-white mb-2">
               Venkata Shivanaga Kumar
-            </h3>
+            </h2>
+                </div>
             </div>
           </div>
 
